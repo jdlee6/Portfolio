1fad1cd5add4e95999bfc4e3eab4127b368058ff
Joe Lee <jdlee6@buffalo.edu>
2021-05-15 14:32:50 -0400

    Updated resume & about section

--- src/components/AboutPaper.js
+++ src/components/AboutPaper.js
@@ -27,9 +27,14 @@ export default function AboutPaper() {
         <Typography variant="h3">
           Hi, my name is <em>Joe</em>.
           <br />
-          <br />I am a <em>Full Stack Web Developer</em> with experience in Typescript & Python. I was born and raised in New York City but I currently reside in San Diego, CA. I <em>love</em> manifesting
-          ideas and transforming them into features and products. I am a fast
-          learner who <em>enjoys</em> exploring new technologies and frameworks.
+          <br />I am a <em>Full Stack Web Developer</em> with experience in Typescript & Python.
+          <br />
+          I was born and raised in New York City and currently reside in Queens, New York.
+          <br />
+          I am someone who <em>loves</em> manifesting
+          ideas and transforming them into features and products. 
+          <br />
+          I am a fast learner who <em>enjoys</em> exploring new technologies and frameworks.
           <br />
         </Typography>
       </ThemeProvider>
